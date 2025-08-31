eff6e005b611314cba29811031663dc21e3cf838
Aaron <Aaron9034@Hotmail.com>
2025-08-30 23:20:34 -0700

    rename "chain" and "candidates"

--- src/project_catalog/galactic_binary.py
+++ src/project_catalog/galactic_binary.py
@@ -3,110 +3,117 @@ from pathlib import Path
 import numpy as np
 from corner import corner
 
-class GalacticBinary:
-    """
-    Container for a posterior chain and its corresponding injection candidates.
-    """
-    def __init__(self, name: str, chain: pd.DataFrame, candidates: pd.DataFrame):
-        self.name = name
-        self.chain = chain
-        self.candidates = candidates
-        self.binary_params = ['Frequency', 'Amplitude', 'Inclination',
+binary_parameters = ['Frequency', 'Amplitude', 'Inclination',
                               'Initial Phase', 'Ecliptic Latitude',
                               'Ecliptic Longitude', 'Polarization',
                               'Frequency Derivative']
-        print(f"Loaded {self.name} with {len(self.chain)} samples and {len(self.candidates)} candidates.")
+sky_location_parameters = ['Ecliptic Latitude', 'Ecliptic Longitude']
+
+def wrap_polarization_angle(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Wrap the polarization angle to be between 0 and pi.
+    """
+    theta = df["Polarization"].to_numpy().squeeze()
+    wrapped = np.abs(np.angle(np.exp(1j * theta)))
+    df["Polarization"] = wrapped
+    return df
+
+
+class GalacticBinary:
+    """
+    Container for a posterior chain samples and their corresponding possible injection matches.
+    """
+    def __init__(self, name: str, posterior: pd.DataFrame, injections: pd.DataFrame):
+        self.name = name
+        self.posterior = posterior
+        self.injections = injections
+        self.binary_params = binary_parameters
+        print(f"Loaded {self.name} with {len(self.posterior)} samples and {len(self.injections)} possible injection matches.")
 
     @classmethod
     def load_feather(cls,
-                      name: str,
-                      chain_dir: str = None,
-                      candidates_dir: str = None):
+                     name: str,
+                     posterior_dir: Path|None = None,
+                     injections_dir: Path|None = None):
         """
-        Load chain and candidates (SNR > 1) from Feather files.
-        name           : base name (e.g. 'LDC0017720857')
-        chain_dir      : path to directory containing '*_chain.feather'
-        candidates_dir : path to directory containing '*_candidates.feather'
-        snr_cut        : whether to look for *_candidates.feather
+        Load posterior samples and possible injection matches (SNR > 1) from Feather files.
+        name : base name (e.g. 'LDC0017720857')
+        posterior_dir : path to directory containing '*_posterior.feather'
+        _dir : path to directory containing '*_injections.feather'
         """
-        if chain_dir is None:
-            chain_dir = Path(__file__).parent.parent.parent / "data/posterior_chains/"
-        if candidates_dir is None:
-            candidates_dir = Path(__file__).parent.parent.parent / "data/injection_candidates/"
-        chain_fp = Path(chain_dir) / f"{name}_chain.feather"
-        suffix = "_candidates.feather"
-        cand_fp = Path(candidates_dir) / f"{name}{suffix}"
+        if posterior_dir is None:
+            posterior_dir = Path(__file__).parent.parent.parent / "data/posterior_chains/"
+        if injections_dir is None:
+            injections_dir = Path(__file__).parent.parent.parent / "data/injection_matches/"
+        posterior_fp = Path(posterior_dir) / f"{name}_posterior.feather"
+        suffix = "_injections.feather"
+        injections_fp = Path(injections_dir) / f"{name}{suffix}"
         try:
-            chain_df = pd.read_feather(chain_fp)
+            posterior_df = pd.read_feather(posterior_fp)
             # TODO (Aaron): Come back and try to use all samples
-            if chain_df.shape[0] > 10_000:
+            if posterior_df.shape[0] > 10_000:
                 thin_factor = 10
-                chain_df = chain_df.iloc[::thin_factor].reset_index(drop=True)
+                posterior_df = posterior_df.iloc[::thin_factor].reset_index(drop=True)
         except FileNotFoundError:
-            raise FileNotFoundError(f"Chain file {chain_fp} not found.")
+            raise FileNotFoundError(f"Posterior file {injections_fp} not found.")
         try:
-            cand_df  = pd.read_feather(cand_fp)
+            injections_df  = pd.read_feather(injections_fp)
         except FileNotFoundError:
-            print(f"Candidates file {cand_fp} not found. Using empty DataFrame.")
-            cand_df = pd.DataFrame(columns=['Frequency', 'Amplitude', 'Inclination',
-                                            'Initial Phase', 'Ecliptic Latitude',
-                                            'Ecliptic Longitude', 'Polarization',
-                                            'Frequency Derivative'])
+            print(f"Injections file {injections_fp} not found. Using empty DataFrame.")
+            injections_df = pd.DataFrame(columns=binary_parameters + ['Name', 'SNR'])
 
-        # Wrap the polarization angle of candidates to be between 0 and pi (to match chain prior)
-        theta = cand_df["Polarization"].to_numpy().squeeze()
-        wrapped = np.abs(np.angle(np.exp(1j * theta)))
-        cand_df["Polarization"] = wrapped
-        return cls(name, chain_df, cand_df)
+        # Wrap the polarization angle of injections to be between 0 and pi (to match posterior prior)
+        injections_df = wrap_polarization_angle(injections_df)
+        return cls(name, posterior_df, injections_df)
 
     def save_feathers(self,
-                      chain_dir: str,
-                      candidates_dir: str):
+                      posterior_dir: str,
+                      injections_dir: str):
         """
-        Save chain and candidates to Feather files (resetting their indices).
+        Save posterior and potential injections matches to Feather files (resetting their indices).
         """
-        Path(chain_dir).mkdir(exist_ok=True, parents=True)
-        Path(candidates_dir).mkdir(exist_ok=True, parents=True)
+        Path(posterior_dir).mkdir(exist_ok=True, parents=True)
+        Path(injections_dir).mkdir(exist_ok=True, parents=True)
 
-        chain_fp = Path(chain_dir) / f"{self.name}_chain.feather"
-        suffix = "_candidates.feather"
-        cand_fp = Path(candidates_dir) / f"{self.name}{suffix}"
+        posterior_fp = Path(posterior_dir) / f"{self.name}_posterior.feather"
+        suffix = "_injections.feather"
+        injections_fp = Path(injections_dir) / f"{self.name}{suffix}"
 
         # Feather requires a default RangeIndex
-        self.chain.reset_index(drop=True).to_feather(chain_fp)
-        self.candidates.reset_index(drop=True).to_feather(cand_fp)
+        self.posterior.reset_index(drop=True).to_feather(posterior_fp)
+        self.injections.reset_index(drop=True).to_feather(injections_fp)
 
-    def get_binary_parameters_chain(self) -> pd.DataFrame:
+    def get_binary_parameters_posterior(self) -> pd.DataFrame:
         """
-        Return a DataFrame with only the galactic binary parameter columns from the chain.
+        Return a DataFrame with only the galactic binary parameter columns from the posterior.
         """
-        return self.chain[self.binary_params]
+        return self.posterior[self.binary_params]
 
-    def get_binary_parameters_candidates(self) -> pd.DataFrame:
+    def get_binary_parameters_injections(self) -> pd.DataFrame:
         """
-        Return a DataFrame with only the galactic binary parameter columns from the candidates.
+        Return a DataFrame with only the galactic binary parameter columns from the possible injection matches.
         """
-        return self.candidates[self.binary_params]
+        return self.injections[self.binary_params]
     
-    def get_sky_location_chain(self) -> pd.DataFrame:
+    def get_sky_location_posterior(self) -> pd.DataFrame:
         """
-        Return a DataFrame with the sky location parameters from the chain.
+        Return a DataFrame with the sky location parameters from the posterior.
         """
-        return self.chain[['Ecliptic Latitude', 'Ecliptic Longitude']]
+        return self.posterior[sky_location_parameters]
 
-    def get_sky_location_candidates(self) -> pd.DataFrame:
+    def get_sky_location_injections(self) -> pd.DataFrame:
         """
-        Return a DataFrame with the sky location parameters from the candidates.
+        Return a DataFrame with the sky location parameters from the possible injection matches.
         """
-        return self.candidates[['Ecliptic Latitude', 'Ecliptic Longitude']]
+        return self.injections[sky_location_parameters]
 
-    def corner_plot(self, candidate_index: int, **kwargs):
+    def corner_plot(self, injection_index: int, **plot_kwargs):
         """
-        Plot the corner plot of the chain.
+        Show the corner plot of the posterior.
         """
-        fig = corner(self.get_binary_parameters_chain(),
-                     truths = self.get_binary_parameters_candidates().iloc[candidate_index],
+        fig = corner(self.get_binary_parameters_posterior(),
+                     truths = self.get_binary_parameters_injections().iloc[injection_index],
                      labels = self.binary_params,
-                     **kwargs)
+                     **plot_kwargs)
         fig.suptitle(self.name)
         return fig
